edd7786a630bba575b3a75569ad8a927709a71c1
sgoenko <sgoenko@gmail.com>
2021-06-10 09:21:26 +0300

    invokeAny

--- Concurrent/src/hay/thread/Runner.java
+++ Concurrent/src/hay/thread/Runner.java
@@ -5,24 +5,31 @@ import java.util.List;
 import java.util.concurrent.*;
 
 public class Runner {
+
+	static Callable callable(String result, long sleepSeconds) {
+		return () -> {
+			TimeUnit.SECONDS.sleep(sleepSeconds);
+			return result;
+		};
+	}
+
 	public static void main(String[] args) throws InterruptedException {
 		ExecutorService executor = Executors.newWorkStealingPool();
 
-		List<Callable<String>> callables = Arrays.asList(
-				() -> "task1",
-				() -> "task2",
-				() -> "task3");
 
-		executor.invokeAll(callables)
-				.stream()
-				.map(future -> {
-					try {
-						return future.get();
-					} catch (Exception e) {
-						throw new IllegalStateException(e);
-					}
-				})
-				.forEach(System.out::println);
+		List<Callable<String>> callables = Arrays.asList(
+				callable("task1", 2),
+				callable("task2", 1),
+				callable("task3", 3)
+		);
+
+		String result = null;
+		try {
+			result = executor.invokeAny(callables);
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		}
+		System.out.println(result);
 
 	}
 }
