d8425b88f72574c8060e93daa74be7e01aa794fb
sgoenko <sgoenko@gmail.com>
2021-06-10 12:44:27 +0300

    Semaphore

--- Concurrent/src/hay/thread/Runner.java
+++ Concurrent/src/hay/thread/Runner.java
@@ -4,32 +4,39 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.StampedLock;
+import java.util.stream.IntStream;
 
 public class Runner {
-	int count = 0;
 
 	public void run() throws InterruptedException {
-		ExecutorService executor = Executors.newFixedThreadPool(2);
-		StampedLock lock = new StampedLock();
+		ExecutorService executor = Executors.newFixedThreadPool(10);
 
-		executor.submit(() -> {
-			long stamp = lock.readLock();
+		Semaphore semaphore = new Semaphore(5);
+
+		Runnable longRunningTask = () -> {
+			boolean permit = false;
 			try {
-				if (count == 0) {
-					stamp = lock.tryConvertToWriteLock(stamp);
-					if (stamp == 0L) {
-						System.out.println("Could not convert to write lock");
-						stamp = lock.writeLock();
-					}
-					count = 23;
+				permit = semaphore.tryAcquire(1, TimeUnit.SECONDS);
+				if (permit) {
+					System.out.println("Semaphore acquired");
+					sleep(5);
+				} else {
+					System.out.println("Could not acquire semaphore");
 				}
-				System.out.println(count);
+			} catch (InterruptedException e) {
+				throw new IllegalStateException(e);
 			} finally {
-				lock.unlock(stamp);
+				if (permit) {
+					semaphore.release();
+				}
 			}
-		});
+		};
+
+		IntStream.range(0, 10)
+				.forEach(i -> executor.submit(longRunningTask));
 
 		stop(executor);
 	}
