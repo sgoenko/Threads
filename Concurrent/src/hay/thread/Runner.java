ab4921bc17f412e02cde07791605b445c8458a5b
sgoenko <sgoenko@gmail.com>
2021-06-10 11:20:22 +0300

    StampedLock

--- Concurrent/src/hay/thread/Runner.java
+++ Concurrent/src/hay/thread/Runner.java
@@ -4,35 +4,32 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.locks.ReadWriteLock;
-import java.util.concurrent.locks.ReentrantLock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
-import java.util.stream.IntStream;
+import java.util.concurrent.locks.StampedLock;
 
 public class Runner {
 
 	public void run() throws InterruptedException {
 		ExecutorService executor = Executors.newFixedThreadPool(2);
 		Map<String, String> map = new HashMap<>();
-		ReadWriteLock lock = new ReentrantReadWriteLock();
+		StampedLock lock = new StampedLock();
 
 		executor.submit(() -> {
-			lock.writeLock().lock();
+			long stamp = lock.writeLock();
 			try {
 				ConcurrentUtils.sleep(1);
 				map.put("foo", "bar");
 			} finally {
-				lock.writeLock().unlock();
+				lock.unlockWrite(stamp);
 			}
 		});
 
 		Runnable readTask = () -> {
-			lock.readLock().lock();
+			long stamp = lock.readLock();
 			try {
 				System.out.println(map.get("foo"));
 				ConcurrentUtils.sleep(1);
 			} finally {
-				lock.readLock().unlock();
+				lock.unlockRead(stamp);
 			}
 		};
 
